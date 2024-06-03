1563f95c741e630587d6b2e9a0373eaeff6d9173
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-06-03 23:17:41 +0530

    Update page.php

--- Custom-theme/page.php
+++ Custom-theme/page.php
@@ -1,11 +1,27 @@
 <?php
 get_header('secondary');
 ?>
-
+<section class="page-wrap">
 <div class="container">
-    <h1><?php the_title();?></h1>
-    <?php get_template_part('includes/section', 'content'); ?>
+    <section class="row">
+        <div class="col-lg-3">
 
+ 
+                <?php if( is_active_sidebar('page-sidebar')):?>
+                    <?php dynamic_sidebar('page-sidebar')?>
+<?php endif;?>
 </div>
-
+<div class="col-lg-9">
+        <h1><?php the_title();?></h1>
+            <?php if(has_post_thumbnail()):?>
+    
+                    
+                <img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail" >
+             <?php endif;?>
+   
+    <?php get_template_part('includes/section', 'content'); ?>
+</div>
+    </section>
+</div>
+</section>
 <?php get_footer();?>
